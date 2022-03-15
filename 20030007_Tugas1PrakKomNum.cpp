7109b26959c0d1b9a916f0bc9474e6819130217b
alyshafatharizkan <97462780+alyshafatharizkan@users.noreply.github.com>
2022-03-15 14:49:44 +0700

    Add files via upload

---
+++ 20030007_Tugas1PrakKomNum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string a;
+	cout<<"Masukkan Suatu Kalimat = ";
+	getline(cin,a);
+	cout<<endl;
+	cout<<"---------------------------"<<endl;
+	cout<<a<<endl;
+	cout<<"---------------------------"<<endl;
+	return 0;
+}
